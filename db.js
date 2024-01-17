f44fc4a826d335d370a444875021bf5848390f62
Manik <manikproch9858@gmail.com>
2024-01-17 15:48:52 +0530

    mongoose connection initiated

---
+++ db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+ const conn= async ()=>{
+    try {
+        const isconnected= await mongoose.connect("mongodb+srv://SimpleGyan:SimpleGyan123@cluster0.ghmq5p1.mongodb.net/")
+        console.log("Successfully connected to the database".bgGreen.white)
+
+    } catch (error) {
+        console.log(error.bgRed.white)
+    }
+
+}
+
+export default conn
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import  Express  from "express";
+import conn from "./db.js"
 
 const app = Express();
 app.get("/",(req,res)=> {
     res.send("<h1>Welcome</h1>");
 })
 
+conn()
 const port=5000
 app.listen(5000,()=>{
     console.log(`Server is running on ${port}`)
 })
+
+
 
 // app.listen(5000)
