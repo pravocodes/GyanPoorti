3c6ec579155391aa8a257f897b8d439075d22dca
Manik <manikproch9858@gmail.com>
2024-01-19 22:53:43 +0530

    register page

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,9 +1,25 @@
-function Login() {
+import React from 'react'
+import "./styles.css"
+
+const Login = () => {
   return (
-    <div>
-      Login PAge
+    <>
+    {/* <img src="./wave.png"></img> */}
+    <div className="login-container">
+      <img src="https://i.postimg.cc/zX8Zbg5P/avatar.png" alt="" />
+      <form action='#'>
+       {/* <label for="username">Username: </label> */}
+       <input type="text" name="username" placeholder="Username" required></input>
+
+       {/* <label for="password">Password: </label> */}
+       <input type="text" name="password" placeholder="Password" required></input>
+       <a href="a">Forgot Password?</a>
+       <button type="submit">Login</button>
+      
+      </form>
     </div>
+    </>
   )
 }
 
-export default Login
+export default Login;
--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,9 +1,36 @@
-function Register() {
-  return (
-    <div>
-      Register Page
-    </div>
-  )
-}
+import React from "react";
+import "./styles.css";
 
-export default Register
+//firstname /lastname/ phone number /nationality/ email /password
+const Register = () => {
+  return (
+    <div className="register-container">
+      <h1>Register Page</h1>
+      <form action='#'>
+       {/* <label for="fullname">Fullname: </label> */}
+       <input type="text" name="firstname" placeholder="Enter your firstname" required></input>
+
+       {/* <label for="emaiid">Email Id:</label> */}
+       <input type="text" name="lastname" placeholder="Enter your lastname" required></input>
+
+       <input type="text" name="phonenumber" placeholder="Enter your number" required></input>
+
+       <p>Choose Your Nationality :</p>
+            <select name="language" id="register-nationality">
+                <option value="indian">Indian</option>
+                <option value="others">Others</option>
+            </select>
+      
+            <input type="text" name="emailid" placeholder="Enter your Email Id" required></input>
+            <input type="text" name="password" placeholder="Enter your password" required></input>
+
+      
+      
+       <button type="submit">Register Now</button>
+      
+      </form>
+    </div>
+  );
+};
+
+export default Register;
